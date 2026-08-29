0f811ea4ea2f118136de1688fc79be21132bc16c
Wahb amir <wahbamir2010@gmail.com>
2026-08-30 03:12:26 +0500

    fix(redis): set redis connection family to 4

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -4,10 +4,15 @@ import { env } from "./env";
 export const redisConnection = new Redis({
   host: env.REDIS_HOST,
   port: Number(env.REDIS_PORT),
+  family: 4,
+
   username: "app",
   password: env.REDIS_PASSWORD || undefined,
+
   maxRetriesPerRequest: null,
+
   tls: {
-    rejectUnauthorized: false, 
+    rejectUnauthorized: false,
+    servername: env.REDIS_HOST,
   },
 });
